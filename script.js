91b660a38f3eaeac16dee209566b586080cec131
SolanoD94 <diana.solano4970@gmail.com>
2023-01-20 18:05:23 -0500

    Current weather function and display. Added Dayjs script. Displayed Forecast Info

--- script.js
+++ script.js
@@ -1,24 +1,66 @@
 var cityFormEl = document.getElementById("city-form");
 var cityInputEl = document.getElementById("cityInput");
-var forecastContainer = document.getElementById("forecast-container");
+var forecastContainer = document.getElementById("forecast-card");
 var searchedCity = document.getElementById("searchedCity");
+var today = dayjs().format("MMMM D, YYYY");
 
-
+console.log(today)
 
 var formSubmit = function(event){
     event.preventDefault();
-
     var cityInput = cityInputEl.value.trim();
 
     if (cityInput) {
         getForecast(cityInput)
+        getWeatherToday(cityInput)
 
-        forecastContainer.textContent = "";
-        cityInputEl.value = "";
+        // forecastContainer.textContent = "";
+        // cityInputEl.value = "";
     }return
 
 }
 
+// Todays Weather API
+var getWeatherToday = function (city_name){
+    var apiUrlNow = "http://api.openweathermap.org/data/2.5/weather?q=" + city_name + "&units=metric&appid=c318abf921e0b2467383d5553bd29182";
+    console.log(apiUrlNow);
+
+    fetch(apiUrlNow, {
+        method: "GET",
+        
+    })
+    .then(function (response){
+        if (response.ok){
+            response.json().then(function(data){
+                console.log(data);
+
+                displayWeatherToday(data, city_name)
+            });
+} else {
+    alert("Error: " + response.statusText);
+}});
+
+
+searchedCity.textContent = city_name + " (" + today + ")";
+};
+
+var displayWeatherToday = function (data){
+    var todayTemp = data.main.temp
+    console.log(todayTemp)
+    temp.textContent = "Temp: " + todayTemp + " °C";
+
+    var todayWind = data.wind.speed
+    console.log(todayWind)
+    wind.textContent = "Wind: " + todayWind + " MPH";
+
+    var todayHumidity = data.main.humidity
+    console.log(todayHumidity)
+    humidity.textContent = "Humidity: " + todayHumidity + "%";
+}
+
+
+
+// 5 Day Forecart API
 var getForecast = function(city_name){
     var apiUrl = "http://api.openweathermap.org/data/2.5/forecast?q=" + city_name + "&cnt=5&units=metric&appid=c318abf921e0b2467383d5553bd29182";
     console.log(apiUrl);
@@ -37,15 +79,17 @@ var getForecast = function(city_name){
         } else {
             alert("Error: " + response.statusText);
         }});
+        
     
 };
 
+// Display 5 Next Forecast Function
 var displayForecast = function (list, city_name){
     if (list.length === 0){
         forecastContainer.textContent = "No results found";
         return;
 }
- searchedCity.textContent = city_name;
+searchedCity.textContent = city_name + " (" + today + ")";
 
 for (var i = 0; i < list.length; i++){
      var cityTemp = list[i].main.temp;
@@ -56,10 +100,26 @@ for (var i = 0; i < list.length; i++){
      console.log(cityWind);
      var timeForecast = list[i].dt_txt;
      console.log(timeForecast)
+
+    //var date = document.getElementById("date");
+
+    var tempEl = document.createElement("p");
+    tempEl.classList = "card-text text-white";
+    tempEl.textContent = "Temp: " + cityTemp + " °C";
+
+    var windEl = document.createElement("p");
+    windEl.classList = "card-text text-white";
+    windEl.textContent = "Wind: " + cityWind + " MPH";
+
+    var humidityEl = document.createElement("p");
+    humidityEl.classList = "card-text text-white";
+    humidityEl.textContent = "Humidity: " + cityHumidity + "%";
+
+
+    forecastContainer.appendChild(tempEl);
+    forecastContainer.appendChild(windEl);
+    forecastContainer.appendChild(humidityEl)
 }
-temp.textContent = "Temp: " + cityTemp + " °C";
-wind.textContent = "Wind: " + cityWind + " MPH";
-humidity.textContent = "Humidity: " + cityHumidity + "%";
 
 
 
