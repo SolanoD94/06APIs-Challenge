2b4e90b8226261f2a4e737844246e2308fe7885e
SolanoD94 <diana.solano4970@gmail.com>
2023-01-28 14:58:52 -0600

    Added funtionality to sidebar buttons

--- script.js
+++ script.js
@@ -3,19 +3,56 @@ var cityInputEl = document.getElementById("cityInput");
 var forecastCard = document.getElementById("forecast-card");
 var searchedCity = document.getElementById("searchedCity");
 var today = dayjs().format("MMMM D, YYYY");
+var sidebarContainer = document.getElementById("sidebarContainer");
 
 console.log(today)
 
 var formSubmit = function(event){
     event.preventDefault();
     var cityInput = cityInputEl.value.trim();
-
+    
     if (cityInput) {
         getForecast(cityInput)
         getWeatherToday(cityInput)
-
     }return
+   
 }
+
+var historySubmit = function(cityInput){
+    if (cityInput) {
+        getForecast(cityInput)
+        getWeatherToday(cityInput)
+    }return
+   
+}
+
+//Create Buttons for every searched city and display in search history column.
+
+sidebarContainer.addEventListener("click", function (event){
+    console.log(event.target)
+    var target = event.target.textContent 
+    sidebarContainer.getAttribute("cityInput")
+    console.log(target)
+    historySubmit(target)
+})
+
+
+var addHistory = function(searchHistory){
+    var searchHistory = JSON.parse(localStorage.getItem("city"))
+
+    var sidebarEl = document.createElement("button");
+    sidebarEl.classList = "btn btn-secondary mt-2 col-12";
+    sidebarEl.setAttribute("cityInput", searchHistory)
+    sidebarEl.innerHTML = searchHistory
+    console.log(sidebarEl)
+
+    cityFormEl.addEventListener("submit", sidebarContainer.appendChild(sidebarEl));
+    
+    //sidebarEl.addEventListener("click", forSubmit());
+    //console.log(sidebarEl.addEventListener("submit", getWeatherToday(city_name)))
+    
+}
+
 
 
 // Todays Weather API
@@ -33,24 +70,28 @@ var getWeatherToday = function (city_name){
                 console.log(data);
 
                 displayWeatherToday(data, city_name)
+                
             });
 } else {
     alert("Error: " + response.statusText);
 }});
 
-
 searchedCity.textContent = city_name + " (" + today + ")";
 
-//Save searched city to Local Storage
-
-localStorage.setItem("city", city_name);
-
-
-
+localStorage.setItem("city", JSON.stringify(city_name));
 };
 
+
+
+
+// Display Today's Weather
 var displayWeatherToday = function (data){
-    
+    var todayIcon = data.weather[0].icon
+    var weatherIconToday = document.getElementById("weatherIconToday")
+    var iconUrlToday = "http://openweathermap.org/img/wn/" + todayIcon + ".png"
+    weatherIconToday.setAttribute("src", iconUrlToday)
+    weatherIconToday.style.width = "4rem"
+
     var todayTemp = data.main.temp
     console.log(todayTemp)
     temp.textContent = "Temp: " + todayTemp + " °C";
@@ -66,7 +107,7 @@ var displayWeatherToday = function (data){
 
 
 
-// 5 Day Forecart API
+// 5 Day Forecast API
 var getForecast = function(city_name){
     var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + city_name + "&cnt=40&units=metric&appid=c318abf921e0b2467383d5553bd29182";
     console.log(apiUrl);
@@ -85,20 +126,20 @@ var getForecast = function(city_name){
         } else {
             alert("Error: " + response.statusText);
         }});
-        
-    
 };
 
 // Display 5 Next Forecast Function
-var displayForecast = function (list, city_name){
+var displayForecast = function (list){
     if (list.length === 0){
         forecastCard.textContent = "No results found";
         return;
 }
-
+var container = document.getElementById("forecast-container");
+    container.innerHTML = "";
+    
 for (var i = 0; i < list.length; i++){
     
-     var dataTime = list[i].dt_txt;
+     var dataTime = dayjs(list[i].dt_txt).format("MMMM D, YYYY HH:mm");
      console.log(dataTime) 
      var cityTemp = list[i].main.temp;
      console.log(cityTemp);
@@ -108,12 +149,14 @@ for (var i = 0; i < list.length; i++){
      console.log(cityWind);
      var icon = list[i].weather[0].icon;
 
-    var container = document.getElementById("forecast-container");
+
     
     var listEl = document.createElement("div");
     listEl.classList = "col card card-body bg-secondary m-1"
-    listEl.textContent = ""
+    listEl.textContent = "";
+    
     container.appendChild(listEl)
+    
     
     var date = document.createElement("h4");
     date.classList = "card-text text-white text-center";
@@ -136,7 +179,6 @@ for (var i = 0; i < list.length; i++){
     weatherIcon.setAttribute("src", iconWeather)
     weatherIcon.classList = "align-self-center"
     weatherIcon.style.width = "3rem"
-    console.log(weatherIcon)
 
     listEl.appendChild(date);
     listEl.appendChild(weatherIcon);
@@ -146,6 +188,8 @@ for (var i = 0; i < list.length; i++){
 }
 
 
-
 }
-cityFormEl.addEventListener("submit", formSubmit)
+
+
+cityFormEl.addEventListener("submit", formSubmit);
+cityFormEl.addEventListener("submit", addHistory);
